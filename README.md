2a15ba419c15fffd4ca82809fe071fb97682de77
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-16 17:03:31 +0200

    added the first upgrade to the js file and added the mission of clickmeme to the readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CLICKMEme
-a clicker that giver you another meme for every (random++) clicks
+### click it untill you make it!
+
+# the CLICKMEme mission:
+
+<br>
+
+having fun clicking on things ?
+with CLICKMEme you can get rewards for clicking AND look at memes at the same time.
+
+[check it out!]
+
--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@ let counter = 0;
 let upgradeOne = 1;
 let upgradeTwo = 0;
 let counterHTML = document.getElementById('theCounter');
+let clickPower = document.getElementById('clickPower');
 
+
+// the first upgrade button
+let upgraderOne = document.getElementById('click-plus').addEventListener('click', () => {
+    upgradeOne += 1;
+    clickPower.innerHTML = upgradeOne;
+
+});
+
+// the incrementor
 let clicker = document.getElementById('clickImage').addEventListener('click',() => {
-    counter += 1;
+    counter += upgradeOne;
     counterHTML.innerHTML = counter;
 });
+
