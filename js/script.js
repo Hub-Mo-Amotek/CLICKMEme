17afa0acfafc4e3670cffaab1823598944836998
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-16 19:13:17 +0200

    added memes for the clicker game, if(succes = true){add more memes}

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 
-
+//variables
 let counter = 0;
 let upgradeOne = 1;
 let upgradeTwo = 0
 let counterHTML = document.getElementById('theCounter');
 let clickPower = document.getElementById('clickPower');
 let autoClickCounter = document.getElementById('auto-click-counter');
+
+// meme pic array
+let theMemes = ["/memeImages/Schermafbeelding 2022-05-16 om 14.30.45.png", "/memeImages/"]
 
 
 
@@ -23,6 +26,10 @@ let upgraderTwo =  document.getElementById('auto-clicker').addEventListener('cli
     upgradeTwo += 1
     autoClickCounter.innerHTML = upgradeTwo;
 });
+// the third upgrade button
+let upgraderThree = document.getElementById('meme-clicker').addEventListener('click', => {
+
+})
 
 
 
