1311d2eb70f8f21f9d9a3ed02c6d339ba923c243
Hub-Mo <Mohamed.elja@hotmail.com>
2022-05-16 18:05:37 +0200

    added extra upgrade button to get the memes, and added the auto clicker to upgradeTwo

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 let counter = 0;
 let upgradeOne = 1;
-let upgradeTwo = 0;
 let counterHTML = document.getElementById('theCounter');
 let clickPower = document.getElementById('clickPower');
 
@@ -11,8 +10,17 @@ let clickPower = document.getElementById('clickPower');
 let upgraderOne = document.getElementById('click-plus').addEventListener('click', () => {
     upgradeOne += 1;
     clickPower.innerHTML = upgradeOne;
-
 });
+
+// the second upgrade button
+let upgradeTwo =  document.getElementById('auto-clicker').addEventListener('click', () => {
+    setInterval(() => {
+        counterHTML.innerHTML = counter += 1;
+    },1000);
+});
+
+
+
 
 // the incrementor
 let clicker = document.getElementById('clickImage').addEventListener('click',() => {
